51939281e9736d765933cce449893178933d0518
curiousDONG <iandzy0426@gmail.com>
2026-03-22 14:02:24 -0400

    Add customer analysis query for music genre sales
    This SQL query analyzes music genres to determine sales and revenue contributions. It joins the InvoiceLine, Track, and Genre tables to provide insights on tracks sold and revenue percentages.

---
+++ customer_analysis.sql
@@ -0,0 +1,16 @@
+-- Q: "Which music genres drive the most sales and revenue?"
+SELECT
+	g.Name,
+	COUNT(il.InvoiceLineId) AS tracks_sold,
+	ROUND(SUM(il.UnitPrice*il.Quantity), 2) AS revenue ,
+	ROUND( 
+		SUM(il.UnitPrice*il.Quantity) / 
+		SUM(SUM(il.UnitPrice*il.Quantity)) OVER() * 100 , 1) AS percent_of_revenue
+FROM InvoiceLine il
+JOIN Track t ON il.TrackId = t.TrackId
+JOIN Genre g ON t.GenreID = g.GenreId
+-- Here, 3 different tables are joined together, because InvoiceLine contains information on sales, but not the track name or genre
+-- through the  Track table, the genre ID can be found for the track, to find genere name
+-- This is done because the genre table contains information on the genre name and ID, but not the sales, and vice versa
+GROUP BY g.GenreID
+ORDER BY revenue DESC;
