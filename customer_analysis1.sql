ba6e14dbe1e13725f3f20996cb63a5734f29f4c3
curiousDONG <iandzy0426@gmail.com>
2026-03-22 14:04:17 -0400

    Add customer analysis SQL queries
    Added SQL queries to analyze customer spending and identify valuable customers and countries with high average order values.

---
+++ customer_analysis1.sql
@@ -0,0 +1,35 @@
+-- Q: "Who are our most valuable customers, and where are they from?"
+-- to do this, I will have to find the customer id with the highest total spending, and find the information of the customer in the customer table 
+SELECT 
+	RANK() OVER (ORDER BY total_spending DESC) AS rank,
+	-- in order to rank each customer in terms of total spending, the total spending first needs to be found
+	name, 
+	Country,
+	total_spending
+FROM(
+	SELECT 
+	c.FirstName || ' ' || c.LastName AS name,
+	-- two variables can be joined together using the '||} notation, while ' ' simply addes a space between the two 
+	c.Country,
+	ROUND(SUM(i.Total), 2) AS total_spending
+	FROM Customer c
+	JOIN Invoice i on c.CustomerId = i.CustomerId
+	-- where the 
+	GROUP BY c.CustomerId 
+)
+-- grouping by Customer ID to pervent duplicating names
+ORDER BY rank
+LIMIT 10; 
+
+-- Q: "Which countries have customers who spend the most per purchase?"
+-- To answer this question meaningfully, we have to exclude the outliers to know more about a country's spending capacity
+-- Therefore, a lower limit for the number of customers per country needs to be applied in order to ensure the analysis is meaningful
+SELECT 
+	BillingCountry,
+	COUNT(*) AS num_orders,
+	ROUND(AVG(Total), 2) AS avg_order_value
+FROM Invoice
+GROUP BY BillingCountry
+HAVING COUNT(*) >= 5
+-- the having argument filter countries with a low number of customers, each data point having 5 or more counts
+ORDER BY avg_order_value DESC;
