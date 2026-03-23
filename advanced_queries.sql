bebc76335475ea65046ca4e4ed6db85aa751b574
curiousDONG <iandzy0426@gmail.com>
2026-03-22 21:53:26 -0400

    Fix comments and typos in advanced_queries.sql
    Corrected comments for clarity and fixed typos in SQL queries.

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,5 +1,5 @@
 -- Q: "How many customers made more than one purchase?"
--- to anwser to questions, and have a full customer loyal break down, I will have to count how many purchases each customer made
+-- To answer questions and have a full customer loyalty breakdown, I will have to count how many purchases each customer made
 SELECT
 	purchase_count,
 	COUNT(*) AS num_customers 
@@ -11,5 +11,16 @@ FROM(
 	--first, the Inovice data is grouped by customer ID to avoid overlaps
 )AS customer_totals
 GROUP BY purchase_count
--- second, the cutomer data is grouped by their purchase count to reveal customer spending habits
+-- second, the customer data is grouped by their purchase count to reveal customer spending habits
 ORDER BY purchase_count;
+
+
+-- Q: "How does our cumulative revenue grow over time — and are there periods where growth stalls?" 
+SELECT
+InvoiceDate,
+Total,
+ROUND (SUM(Total) OVER(ORDER BY InvoiceDate), 2)  AS running_total
+-- The line above is the backbone to this question. Putting "ORDER BY" inside the OVER() tells the window function to sum progressively
+-- sum all the `Total` values from the very first row up to and including the current row, in date order
+FROM Invoice
+GROUP BY InvoiceDate;
