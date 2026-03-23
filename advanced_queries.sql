75becaa62c0776378cbfa1d86362bad64fbd4891
curiousDONG <iandzy0426@gmail.com>
2026-03-22 20:48:34 -0400

    Implement query for customer purchase counts
    Add SQL query to count customers with multiple purchases.

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,1 +1,15 @@
-
+-- Q: "How many customers made more than one purchase?"
+-- to anwser to questions, and have a full customer loyal break down, I will have to count how many purchases each customer made
+SELECT
+	purchase_count,
+	COUNT(*) AS num_customers 
+FROM(
+	SELECT CustomerId,
+	COUNT(*) AS purchase_count
+	FROM Invoice
+	GROUP BY CustomerId
+	--first, the Inovice data is grouped by customer ID to avoid overlaps
+)AS customer_totals
+GROUP BY purchase_count
+-- second, the cutomer data is grouped by their purchase count to reveal customer spending habits
+ORDER BY purchase_count;
